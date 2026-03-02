1e2a970969d2b7e701e7bfc8696189cf1b26e6e5
giihdbem <gidbem2009@gmail.com>
2026-03-01 21:13:21 -0300

    criei o método usar()

--- ex1.py
+++ ex1.py
@@ -3,9 +3,22 @@ class Celular:
         self.marca = marca
         self.modelo = modelo
         self.bateria = 100
-
+    
+    def usar(self):
+        print('Usando celular')
+        self.bateria -= 10.3 
+    
 celular1 = Celular('Apple', '14 pro')
 celular2 = Celular('Samsung', 'A23')
 
 print(celular1.marca)
 print(celular2.bateria)
+
+
+#exercício 2 (def usar)
+
+celular = Celular('Apple', '11')
+
+celular.usar()
+celular.usar()
+print(celular.bateria)
