a44d8d6b9840f3ac97d563d44188d5c567b70152
giovanna.bem <gidbem2009@gmail.com>
2026-03-03 15:00:48 -0300

    editei o código

--- ex1.py
+++ ex1.py
@@ -1,24 +1,11 @@
 class Celular:
-    def __init__(self, marca, modelo):
+    def __init__(self, marca, modelo, bateria=100):
         self.marca = marca
         self.modelo = modelo
-        self.bateria = 100
+        self.bateria = bateria
     
     def usar(self):
         print('Usando celular')
-        self.bateria -= 10.3 
-    
-celular1 = Celular('Apple', '14 pro')
-celular2 = Celular('Samsung', 'A23')
-
-print(celular1.marca)
-print(celular2.bateria)
+        self.bateria -= 10
 
 
-#exercício 2 (def usar)
-
-celular = Celular('Apple', '11')
-
-celular.usar()
-celular.usar()
-print(celular.bateria)
